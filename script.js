d8ff51b4d04742004ed48b177702778eefcddadf
Xol3 <Xoalxiyetl@gmail.com>
2018-08-22 12:32:50 -0500

    finished BG selector

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+//
 var body = document.getElementById("gradient");
+
+// body.style.background = "red";
 
 function setGradient() {
 	body.style.background =
